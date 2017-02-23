99b2c24340eee9e1f26ff85bfebd69b14fbe7bb8
Minh-Long Do <minhlong.langos@gmail.com>
2017-02-23 17:43:35 +0000

    Skip unittest with personal github token

--- github/tests.py
+++ github/tests.py
@@ -3,12 +3,14 @@
 
 from django.test import TestCase
 from .api import GitHubAPI
+from unittest import skip
 
 
 class GithubAPITest(TestCase):
     def setUp(self):
         self.github_api = GitHubAPI()
 
+    @skip(reason="If this is run on travis, the personal token will be automatically deleted")
     def test_get_user_repos(self):
         data = self.github_api.fetch_all_user_repos()
 
