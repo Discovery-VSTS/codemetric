2240225500a7683c6d7273d2ab2f4ee9b6fa4879
Minh-Long Do <minhlong.langos@gmail.com>
2017-02-23 17:17:37 +0000

    Add Github api to retrieve repos of a Github user

---
+++ github/api.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+
+
+class GitHubAPI:
+    def __init__(self, username='minhlongdo', api_token='73d9aa66bf7747d12632c2fab037892f2797131c',
+                 base_url='https://api.github.com'):
+        self.username = username
+        self.api_token = api_token
+        self.base_url = base_url
+
+    def set_username(self, username):
+        self.username = username
+
+    def set_api_token(self, api_token):
+        self.api_token = api_token
+
+    def fetch_all_user_repos(self):
+        request_url = self.base_url + "/users/{}/repos".format(self.username)
+
+        headers = {
+            'Authorization': self.api_token,
+            'Accept': 'application/vnd.github.inertia-preview+json'
+        }
+
+        try:
+            r = requests.get(request_url, headers=headers)
+
+            print("Received data: ", r)
+
+            if r.status_code == 200:
+                print("Received data: ", r.json())
+                return r.json()
+
+            r.raise_for_status()
+        except Exception as e:
+            print("Something unexpected happened")
+            raise Exception(e)
