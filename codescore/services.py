58bb9b602c6ee0c619285133bf5e85463af640e7
Minh-Long Do <minhlong.langos@gmail.com>
2017-02-24 15:47:15 +0000

    Cache github repo with association with codeclimate's id

--- codescore/services.py
+++ codescore/services.py
@@ -14,7 +14,24 @@ class CodeMetricService:
         self.codeclimate_api = CodeClimateAPI(base_url=codeclimate_base_url, api_token=codeclimate_token)
         self.github_api = GitHubAPI(api_token=github_token, username=github_username, base_url=github_base_url)
 
-    def map_github_with_codeclimate(self):
+    def map_github_with_codeclimate_in_cache(self):
         github_repos = self.github_api.fetch_all_user_repos()
 
-        self.codeclimate_api.retrieve_repo_metrics()
+        for github_repo in github_repos:
+            repo_name = github_repo['full_name']
+            repo_id = self.codeclimate_api.retrieve_repo_id(repo_name)
+
+            cache.set(repo_name, repo_id, 60 * 60)
+
+    def retrieve_repo_id_using_repo_name(self, repo_name):
+        repo_id = cache.get(repo_name)
+
+        if repo_id is None:
+            repo_id = self.codeclimate_api.retrieve_repo_id(repo_name=repo_name)
+            if repo_id is None:
+                return None
+            else:
+                cache.set(repo_name, repo_id, 60 * 60)
+                return repo_id
+
+        return repo_id
